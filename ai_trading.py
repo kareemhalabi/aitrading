9f56d4aafb2a524f30cbbe937ed409304879e9dd
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-04-17 10:17:02 -0400

    WORKS try renaming

--- ai_trading.py
+++ ai_trading.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+ai_trading = Flask(__name__)
 
-@app.route('/')
+@ai_trading.route('/')
 def index():
     return 'Hello World!'
 
 if __name__ == "__main__":
-    app.run()
+    ai_trading.run()
