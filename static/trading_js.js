8db765da0d32a30ecc5618e21523d1291a5d91dd
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-06-06 19:26:48 -0400

    Refactor to JQuery

--- static/trading_js.js
+++ static/trading_js.js
@@ -7,58 +7,48 @@
  */
 function searchByIsin() {
 
-    document.getElementById("isin_search_icon").style.display = "none";
-    document.getElementById("isin_loader").style.display = "inline-block";
-    var isinSearch = {"ISIN":document.getElementById("isin").value,
-        "currency":document.getElementById("currency").value};
+    $("#isin_search_icon").css("display", "none");
+    $("#isin_loader").css("display", "inline-block");
+    var isinSearch = {ISIN: $("#isin").val(),
+        currency: $("#currency").val()};
 
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if(this.readyState == 4) {
-            if(this.status == 200) {
-                populateTradeForm(JSON.parse(this.responseText));
+    $.ajax({url: "search_by_isin", data: isinSearch, timeout: 20000,
+            success: function(response) {
+                populateTradeForm(response);
+            },
+            error: function (error) {
+                $("#error_message").text("Error " + error.status + ": " + error.statusText).parent().show();
+            },
+            complete: function () {
+                    $("#isin_search_icon").css("display", "inline-block");
+                    $("#isin_loader").css("display", "none");
             }
-            else { // Error occurred
-                var errorText = document.getElementById("error_message");
-                errorText.innerHTML = "Error "+this.status + ": " + this.statusText;
-                errorText.parentNode.style.display = "block";
-            }
-            document.getElementById("isin_search_icon").style.display = "inline-block";
-            document.getElementById("isin_loader").style.display = "none";
-        }
-    };
-    xhttp.open("GET", "search_by_isin?"+encodeQueryData(isinSearch), true);
-    xhttp.send();
+            });
 }
+
 /**
  * Searches for a security by ticker. Note only alphabetic characters and the period
  * are allowed.
  */
 function searchByTicker() {
 
-    document.getElementById("ticker_search_icon").style.display = "none";
-    document.getElementById("ticker_loader").style.display = "inline-block"
-    var tickerSearch = {"ticker":document.getElementById("ticker").value,
-        "currency":document.getElementById("currency").value};
+    $("#ticker_search_icon").css("display", "none");
+    $("#ticker_loader").css("display", "inline-block");
+    var tickerSearch = {ticker: $("#ticker").val(),
+        currency: $("#currency").val()};
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if(this.readyState == 4) {
-            if(this.status == 200) {
-                populateTradeForm(JSON.parse(this.responseText));
-            }
-            else { // Error occurred
-                var errorText = document.getElementById("error_message");
-                errorText.innerHTML = "Error "+this.status + ": " + this.statusText;
-                errorText.parentNode.style.display = "block";
-            }
-            document.getElementById("ticker_search_icon").style.display = "inline-block";
-            document.getElementById("ticker_loader").style.display = "none";
+    $.ajax({url: "search_by_ticker", data: tickerSearch, timeout: 20000,
+        success: function(response) {
+            populateTradeForm(response);
+        },
+        error: function (error) {
+            $("#error_message").text("Error " + error.status + ": " + error.statusText).parent().show();
+        },
+        complete: function () {
+                $("#ticker_search_icon").css("display", "inline-block");
+                $("#ticker_loader").css("display", "none");
         }
-    };
-    xhttp.open("GET", "search_by_ticker?"+encodeQueryData(tickerSearch), true);
-    xhttp.send();
+        });
 }
 
 /**
@@ -66,112 +56,109 @@ function searchByTicker() {
  * @param security object representing security info
  */
 function populateTradeForm(security) {
-    var errorText = document.getElementById("error_message");
     if(security.error.length > 0) {
-        errorText.innerHTML = security.error;
-        errorText.parentNode.style.display = "block";
+        $("#error_message").text(security.error)
+            .parent().show();
     } else {
-        errorText.parentNode.style.display = "none";
-        document.getElementById("isin").value = security.ISIN;
-        document.getElementById("ticker").value = security.ticker;
-        document.getElementById("sec_name").value = security.security;
-        document.getElementById("price").value = security.last_close_price.toFixed(2);
+        $("#error_message").parent().hide();
+        $("#isin").val(security.ISIN);
+        $("#ticker").val(security.ticker);
+        $("#sec_name").val(security.security);
+        $("#price").val(security.last_close_price.toFixed(2));
     }
 }
 
 /**
  * Validates ISIN input
- * @param field The ISIN field object
+ * @param $field The ISIN field object
  */
-function isinInputHandler(field) {
-    field.value = field.value.replace(/[^a-zA-Z0-9]/g,"").toUpperCase();
+function isinInputHandler($field) {
+    $field.val(function(i, oldval) {
+        return oldval.replace(/[^a-zA-Z0-9]/g,"").toUpperCase()
+    });
 
-    var parentDiv = document.getElementById("isin_group");
-    var isinBtn = document.getElementById("isin_btn");
-
-    if(field.value.length > 0) {
-        if(validateISIN(field.value)) {
-            parentDiv.className = "form-group has-success";
-            isinBtn.disabled = false;
+    if($field.val().length > 0) {
+        if(validateISIN($field.val())) {
+            $("#isin_group").attr("class", "form-group has-success");
+            $("#isin_btn").prop("disabled", false);
         } else {
-            parentDiv.className = "form-group has-error";
-            isinBtn.disabled = true;
+            $("#isin_group").attr("class", "form-group has-error");
+            $("#isin_btn").prop("disabled", true);
         }
     } else {
-        parentDiv.className = "form-group";
-        isinBtn.disabled = true;
+        $("#isin_group").attr("class", "form-group");
+        $("#isin_btn").prop("disabled", true);
     }
-
 }
 
 /**
  * Validates ticker input
- * @param field The ticker field object
+ * @param $field The ticker field object
  */
-function tickerInputHandler(field) {
-    field.value = field.value.replace(/-/g,".").replace(/[^a-zA-Z.]/g,"").toUpperCase();
+function tickerInputHandler($field) {
+    $field.val(function(i, oldval) {
+        return oldval.replace(/-/g,".").replace(/[^a-zA-Z.]/g,"").toUpperCase()
+    });
 
-    var parentDiv = document.getElementById("ticker_group");
-    var tickerBtn = document.getElementById("ticker_btn");
-
-    if(field.value.length > 0) {
-        if((/^([A-Z]+)(\.[A-Z]{1,2})?$/).test(field.value)) {
-            parentDiv.className = "form-group has-success";
-            tickerBtn.disabled = false;
+    if($field.val().length > 0) {
+        if((/^([A-Z]+)(\.[A-Z]{1,2})?$/).test($field.val())) {
+            $("#ticker_group").attr("class", "form-group has-success");
+            $("#ticker_btn").prop("disabled", false);
         } else {
-            parentDiv.className = "form-group has-error";
-            tickerBtn.disabled = true;
+            $("#ticker_group").attr("class", "form-group has-error");
+            $("#ticker_btn").prop("disabled", true);
         }
-
     } else {
-        parentDiv.className = "form-group";
-        tickerBtn.disabled = true;
+        $("#ticker_group").attr("class", "form-group");
+        $("#ticker_btn").prop("disabled", true);
     }
 }
 
 /**
  * Validates shares input
- * @param field The input field object
+ * @param $field The input field object
  */
-function sharesInputHandler(field) {
+function sharesInputHandler($field) {
     // Strip non-numeric for browsers without "number" support
-    field.value = field.value.replace(/[^0-9]/,"");
-    var parentDiv = document.getElementById("shares_group");
+    $field.val(function (i, oldval) {
+        return oldval.replace(/[^0-9]/,"");
+    });
 
-    if(field.value.length > 0) {
-        if(parseInt(field.value) > 0) {
-            parentDiv.className = "form-group has-success";
+    if($field.val().length > 0) {
+        if(parseInt($field.val()) > 0) {
+            $("#shares_group").attr("class", "form-group has-success");
             updateTotal(false);
         } else {
-            parentDiv.className = "form-group has-error";
+            $("#shares_group").attr("class","form-group has-error");
             updateTotal(true);
         }
     } else {
-        parentDiv.className = "form-group";
+        $("#shares_group").attr("class", "form-group");
         updateTotal(true);
     }
 }
 
 /**
  * Validates price input
- * @param field The input field object
+ * @param $field The input field object
  */
-function priceInputHandler(field) {
+function priceInputHandler($field) {
     // Strip non-numeric for browsers without "tel" support
-    field.value = field.value.replace(/[^0-9.]/g,"");
-    var parentDiv = document.getElementById("price_group");
+    $field.val(function (i, oldval) {
+        return oldval.replace(/[^0-9.]/,"");
+    });
 
-    if(field.value.length > 0) {
+    if($field.val().length > 0) {
 
-        if ((/^\d+\.?\d{0,2}$/).test(field.value) && parseFloat(field.value) > 0) {
-            parentDiv.className = "form-group has-success";
+        if ((/^\d+\.?\d{0,2}$/).test($field.val()) && parseFloat($field.val()) > 0) {
+            $("#price_group").attr("class", "form-group has-success");
             updateTotal(false);
         } else {
-            parentDiv.className = "form-group has-error";
+            $("#price_group").attr("class","form-group has-error");
             updateTotal(true);
         }
     } else {
-        parentDiv.className = "form-group";
+        $("#price_group").attr("class", "form-group");
         updateTotal(true);
     }
 }
@@ -181,28 +168,14 @@ function priceInputHandler(field) {
  * @param reset If true, resets total field to 0
  */
 function updateTotal(reset) {
-    var totalField = document.getElementById("total"),
-        sharesField = document.getElementById("shares"),
-        priceField = document.getElementById("price");
 
-    var totalValue = sharesField.value * priceField.value;
+    var totalValue = $("#shares").val() * $("#price").val();
+
     if(!(isNaN(totalValue) || reset)) {
-        totalField.value = totalValue.toLocaleString("en-US", {style: "currency", currency: "USD"}).substring(1);
+        $("#total").val(totalValue.toLocaleString("en-US", {style: "currency", currency: "USD"}).substring(1));
     } else {
-        totalField.value = "0.00";
+        $("#total").val("0.00");
     }
-}
-
-/**
- * Transforms a JS object into a URL query
- * @param data the JS object to encode
- * @returns {string} query data
- */
-function encodeQueryData(data) {
-   var params = [];
-   for (var d in data)
-     params.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
-   return params.join('&');
 }
 
 /**
