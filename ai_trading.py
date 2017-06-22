e6c540d63548b3ba809cb688578bfa1f0cbac1f3
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-06-22 17:36:15 -0400

    noscript.html supports auto redirect when page reloads

--- ai_trading.py
+++ ai_trading.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, Markup
 from morningstar_crawler import *
 
 ai_trading = Flask(__name__)
+
 
 @ai_trading.route('/')
 def index():
@@ -10,16 +11,26 @@ def index():
 
 @ai_trading.route('/noscript')
 def no_script():
-    return ai_trading.send_static_file("noscript.html")
+    redir = request.args.get("redirect")
+
+    # Prevent erroneous redirects
+    if redir[0] != "/":
+        redirect_srcipt = Markup("")
+    else:
+        redirect_srcipt = Markup('window.location.replace("%s");' % redir)
+
+    return render_template("noscript.html", redirect=redirect_srcipt)
 
 
 @ai_trading.route('/search_by_isin', methods=["GET"])
 def search_by_isin():
     return jsonify(find_by_isin(request.args.get("isin"), request.args.get("currency")))
 
+
 @ai_trading.route('/search_by_ticker', methods=["GET"])
 def search_by_ticker():
     return jsonify(find_by_ticker(request.args.get("ticker"), request.args.get("currency")))
+
 
 if __name__ == "__main__":
     ai_trading.run(debug=True)
