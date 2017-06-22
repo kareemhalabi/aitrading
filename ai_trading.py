c20661f45cfb2410199fc14ce6aacabadf2d8cf1
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-06-22 15:39:55 -0400

    Added noscript redirect

--- ai_trading.py
+++ ai_trading.py
@@ -8,6 +8,11 @@ def index():
     return ai_trading.send_static_file("trade.html")
 
 
+@ai_trading.route('/noscript')
+def no_script():
+    return ai_trading.send_static_file("noscript.html")
+
+
 @ai_trading.route('/search_by_isin', methods=["GET"])
 def search_by_isin():
     return jsonify(find_by_isin(request.args.get("isin"), request.args.get("currency")))
