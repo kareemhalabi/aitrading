11a7b9fa2f3639fc74c307cfd8d33b5e299d803d
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-06-24 17:41:04 -0400

    Refactor views to separate file

--- ai_trading.py
+++ ai_trading.py
@@ -1,36 +1,10 @@
-from flask import Flask, jsonify, request, render_template, Markup
-from morningstar_crawler import *
+import os
+from flask import Flask
 
 ai_trading = Flask(__name__)
+ai_trading.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
 
-
-@ai_trading.route('/')
-def index():
-    return render_template("trade.html")
-
-
-@ai_trading.route('/noscript')
-def no_script():
-    redir = request.args.get("redirect")
-
-    # Prevent erroneous redirects
-    if redir[0] != "/":
-        redirect_srcipt = Markup("")
-    else:
-        redirect_srcipt = Markup('window.location.replace("%s");' % redir)
-
-    return render_template("noscript.html", redirect=redirect_srcipt)
-
-
-@ai_trading.route('/search_by_isin', methods=["GET"])
-def search_by_isin():
-    return jsonify(find_by_isin(request.args.get("isin"), request.args.get("currency")))
-
-
-@ai_trading.route('/search_by_ticker', methods=["GET"])
-def search_by_ticker():
-    return jsonify(find_by_ticker(request.args.get("ticker"), request.args.get("currency")))
-
+from views import *
 
 if __name__ == "__main__":
-    ai_trading.run(debug=True)
+    ai_trading.run()
---
+++ views.py
@@ -0,0 +1,33 @@
+from flask import render_template, request, jsonify
+from markupsafe import Markup
+
+from ai_trading import ai_trading
+from morningstar_crawler import find_by_isin, find_by_ticker
+
+
+@ai_trading.route('/')
+def index():
+    return render_template("trade.html")
+
+
+@ai_trading.route('/noscript')
+def no_script():
+    redir = request.args.get("redirect")
+
+    # Prevent erroneous redirects
+    if redir[0] != "/":
+        redirect_srcipt = Markup("")
+    else:
+        redirect_srcipt = Markup('window.location.replace("%s");' % redir)
+
+    return render_template("noscript.html", redirect=redirect_srcipt)
+
+
+@ai_trading.route('/search_by_isin', methods=["GET"])
+def search_by_isin():
+    return jsonify(find_by_isin(request.args.get("isin"), request.args.get("currency")))
+
+
+@ai_trading.route('/search_by_ticker', methods=["GET"])
+def search_by_ticker():
+    return jsonify(find_by_ticker(request.args.get("ticker"), request.args.get("currency")))
