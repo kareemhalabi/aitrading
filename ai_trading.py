659dca252db4810e9846317f2cf461f1d668354c
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-06-25 23:50:23 -0400

    Preliminary Admin page working

--- ai_trading.py
+++ ai_trading.py
@@ -1,10 +1,26 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 ai_trading = Flask(__name__)
 ai_trading.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
+ai_trading.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+db = SQLAlchemy(ai_trading)
+
+class AuthorizedUser(db.Model):
+    email = db.Column(db.String(255), primary_key=True, unique=True)
+    account = db.Column(db.String(12))
+
+    def __init__(self, email, account):
+        self.email = email
+        self.account = account
+
+    def __repr__(self):
+        return '<User %r, Account %r>' % (self.email, self.account)
 
 from views import *
 
 if __name__ == "__main__":
-    ai_trading.run()
+    db.create_all()
+    ai_trading.run(host= '0.0.0.0')
--- views.py
+++ views.py
@@ -1,14 +1,50 @@
+import re
 from flask import render_template, request, jsonify
 from markupsafe import Markup
 
-from ai_trading import ai_trading
+from ai_trading import ai_trading, db, AuthorizedUser
 from morningstar_crawler import find_by_isin, find_by_ticker
 
+from sqlalchemy.exc import IntegrityError
 
 @ai_trading.route('/')
 def index():
     return render_template("trade.html")
 
+@ai_trading.route('/admin')
+def admin():
+    authorized_users = AuthorizedUser.query.order_by(AuthorizedUser.account).all()
+    return render_template("admin.html", authorized_users=authorized_users)
+
+@ai_trading.route('/admin/add_authorized_user', methods=['POST'])
+def add_authorized_user():
+
+    # Email regex from https://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+    email_pattern = re.compile(
+        r'^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+    account_pattern = re.compile(r'^[A-Z0-9]{12}$')
+
+    user = request.get_json()
+    if(email_pattern.match(user['email']) and account_pattern.match(user['account'])):
+        try:
+            db.session.add(AuthorizedUser(**user))
+            db.session.commit()
+            return ""
+        except IntegrityError as e:
+            if(str(e).find("duplicate") != -1):
+                return "User already exists"
+            else:
+                return str(e)
+        except Exception as e:
+            return str(e)
+    else:
+        return "Invalid email/account"
+
+@ai_trading.route('/admin/delete_authorized_user', methods=['POST'])
+def delete_authorized_user():
+    AuthorizedUser.query.filter_by(email=request.get_json()['email']).delete()
+    db.session.commit()
+    return ""
 
 @ai_trading.route('/noscript')
 def no_script():
