cf801acff20f866d81d3fa8a85ff0c3d45f59ffe
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-07-15 12:49:44 -0400

    Change Authorized User to include id as primary key

--- aitrading/models.py
+++ aitrading/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class AuthorizedUser(models.Model):
-    email = models.EmailField(primary_key=True)
+    email = models.EmailField()
     account = models.CharField(max_length=12)
 
     def __str__(self):
