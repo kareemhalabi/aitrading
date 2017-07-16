b63eec4c15139e77663f7294b84cbfa4a8ae215a
Kareem Halabi <kareem.halabi@mail.mcgill.ca>
2017-07-16 00:50:12 -0400

    Mobile friendly auth pages and mobile friendly group info user display boxes

--- aitrading/admin.py
+++ aitrading/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import AuthorizedUser
 
-admin.site.register(AuthorizedUser)
+
+@admin.register(AuthorizedUser)
+class AuthorizedUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'account')
+    list_filter = ('account',)
