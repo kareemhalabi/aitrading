9bf0515f3382b109a25d1288f096f0e15eaeccd6
Kareem Halabi <kareemhalabi24@gmail.com>
2017-10-12 12:50:19 -0400

    Hacky way of keeping Heroku persistent storage

--- aitrading/sftp/sftp_pull.py
+++ aitrading/sftp/sftp_pull.py
@@ -6,7 +6,37 @@ import datetime
 
 reports_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'reports')
 
+def get_put_dev(method):
+    # Set up connection
+    sock = socks.socksocket()
+
+    username = 'wbnbno04'
+    key = paramiko.DSSKey.from_private_key_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'id_dsa'))
+    host = 'devftp5.pershing.com'
+    port = 22
+
+    # Connect to server
+    sock.connect((host, port))
+    transport = paramiko.Transport(sock)
+    transport.connect(username=username, pkey=key)
+    sftp = paramiko.SFTPClient.from_transport(transport)
+
+    if method=='GET':
+        # Delete current directory
+        for file in os.listdir(reports_dir):
+            os.remove(os.path.join(reports_dir, file))
+
+        # Get new directory
+        for file in sftp.listdir():
+            sftp.get(file, os.path.join(reports_dir, file))
+    elif method=='PUT':
+        for file in os.listdir(reports_dir):
+            sftp.put(os.path.join(reports_dir, file), file)
+
+
 def fetch_reports():
+
+    get_put_dev('GET')
 
     # Ignore on a weekend (Saturday is day 5, Sunday is day 6)
     if datetime.datetime.now().weekday() >= 5:
@@ -21,7 +51,7 @@ def fetch_reports():
     print("Current " + date_string + " reports " + str(current_reports))
 
     # Fetch files if not up to date or none at all
-    if len(current_reports) != len(all_reports) or len(all_reports) == 0:
+    if len(current_reports) != len(all_reports)-1 or len(all_reports) == 0: #Ignore the IGNORE text in length of all reports
 
         # Set up connection
         sock = socks.socksocket()
@@ -65,3 +95,5 @@ def fetch_reports():
             for old_file in old_files:
                 print("Deleting: " + str(os.path.join(reports_dir, old_file)))
                 os.remove(os.path.join(reports_dir, old_file))
+
+        get_put_dev('PUT')
