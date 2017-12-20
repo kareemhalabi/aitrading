692a18134da47b9388d154249dc14e922b2462cf
Kareem Halabi <kareemhalabi24@gmail.com>
2017-12-19 21:37:47 -0500

    Fix bug with pending trades being mishandled as cash
    (Cash not always first two rows if pending trades is an item

--- aitrading/sftp/portfolio_scraper.py
+++ aitrading/sftp/portfolio_scraper.py
@@ -1,4 +1,3 @@
-import csv
 import re
 import psycopg2, psycopg2.extras
 from aitrading.sftp.pull_reports import get_db_conn
@@ -10,28 +9,47 @@ def get_portfolio(group_account):
 
     dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    # Filter by account number
-    dict_cur.execute("""SELECT "As Of Date", "Local Currency Code", "Ticker", "ISIN", "Security Description 1",
-                        "Security Description 2", "Shares/Par", "Local Price", "Local Market Value"
-                        FROM report_trdsht_asset_and_accrual_detail WHERE "Reporting Account Number" = '%s' """ % group_account)
-
-    # Get all rows
-    portfolio_rows = dict_cur.fetchall()
-
     portfolio = {}
 
-    # Get As of date (Column E)
-    portfolio["as_of_date"] = portfolio_rows[0]["As Of Date"]
+    # Get CAD Cash
+    dict_cur.execute("""SELECT "Local Market Value"
+                        FROM report_trdsht_asset_and_accrual_detail
+                        WHERE "Security Description 1" = 'CASH' AND  "Reporting Account Number" = %s;""",
+                        (group_account,))
 
-    # First two rows are CAD and USD cash balances
-    portfolio["CAD_cash"] = float(re.sub(',', '', portfolio_rows[0]["Local Market Value"]))
-    del portfolio_rows[0]
-    portfolio["USD_cash"] = float(re.sub(',', '', portfolio_rows[0]["Local Market Value"]))
-    del portfolio_rows[0]
+    portfolio["CAD_cash"] = float(re.sub(',', '', dict_cur.fetchone()["Local Market Value"]))
+
+    # Get USD Cash
+    dict_cur.execute("""SELECT "Local Market Value"
+                        FROM report_trdsht_asset_and_accrual_detail
+                        WHERE "Security Description 1" = 'NON-BASE CURRENCY' AND  "Reporting Account Number" = %s;""",
+                        (group_account,))
+
+    portfolio["USD_cash"] = float(re.sub(',', '', dict_cur.fetchone()["Local Market Value"]))
+
+    # Get conversion rate
+    dict_cur.execute("""SELECT "Exchange Rate"
+                        FROM report_trdsht_asset_and_accrual_detail
+                        WHERE "Asset Category" != 'CASH & CASH EQUIVALENTS'
+                        AND "Local Currency Code" = 'USD';""")
+
+    portfolio["fx_rate"] = float(dict_cur.fetchone()["Exchange Rate"])
+
+    # Get securities
+    dict_cur.execute("""SELECT "As Of Date", "Local Currency Code", "Ticker", "ISIN", "Security Description 1",
+                        "Security Description 2", "Shares/Par", "Local Price", "Local Market Value"
+                        FROM report_trdsht_asset_and_accrual_detail
+                        WHERE "Reporting Account Number" = %s AND "Security Description 1" != 'NON-BASE CURRENCY'
+                        AND "Security Description 1" != 'CASH';""", (group_account,))
+
+    securities = dict_cur.fetchall()
+
+    # Get As of date (Column E)
+    portfolio["as_of_date"] = securities[0]["As Of Date"]
 
     # Format security info from string rows
     portfolio["securities"] = []
-    for row in portfolio_rows:
+    for row in securities:
         portfolio["securities"].append({
             'currency': row["Local Currency Code"],
             'ticker': row["Ticker"],
